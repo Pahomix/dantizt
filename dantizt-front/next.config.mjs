79ee09ffc78f5dc245eab10d01e611d62578ea72
Daniil Seleznev <pahomix@inbox.ru>
2025-05-15 03:31:26 +0300

    Исправлены проблемы со сборкой фронтенда: обновлен next.config.mjs и Dockerfile

--- dantizt-front/next.config.mjs
+++ dantizt-front/next.config.mjs
@@ -18,7 +18,9 @@ const nextConfig = {
     ];
   },
   experimental: {
-    serverActions: true,
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'dantizt.ru', 'www.dantizt.ru'],
+    },
   },
 };
 
